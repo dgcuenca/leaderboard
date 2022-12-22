412f13a3533c7d040e4303c9fa3ca61fc23166f7
diego <dgcuenca@gmail.com>
2022-12-21 20:18:49 -0500

    Consume path games/score get and post

--- src/index.js
+++ src/index.js
@@ -6,27 +6,45 @@ const btnAdd = document.getElementById('btnAdd');
 const name = document.getElementById('name');
 const score = document.getElementById('score');
 const scoreContainer = document.getElementById('score-container');
-const leaderboard = [];
-let idgame={};
+// let idgame={};
 
-fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-  method: 'POST',
-  body: JSON.stringify({ 
-    "name": "Rhyming" 
-  }),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((gameid) => {
-    idgame = gameid;
-    console.log(idgame);
-  });
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+//   method: 'POST',
+//   body: JSON.stringify({ 
+//     "name": "Rhyming" 
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((gameid) => {
+//     // idgame = gameid;
+//     console.log(gameid);
+//   });
+
+
+
 
 btnAdd.addEventListener('click', (event) => {
   event.preventDefault();
-  const newScore = new Score(name.value, score.value);
-  leaderboard.push(newScore);
-  scoreContainer.innerHTML = display(leaderboard);
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/PaP8nHWYVL0mOujbpMVo/scores/', {
+    method: 'POST',
+    body: JSON.stringify({
+      "user": `"${name.value}"`,
+      "score": `${score.value}`
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((gamecreated) => { console.log(gamecreated) });
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/PaP8nHWYVL0mOujbpMVo/scores/')
+    .then((response) => response.json())
+    .then((leaderboard) => {
+      console.log(leaderboard.result[0].score);
+      scoreContainer.innerHTML = display(leaderboard.result);
+    });
 });
