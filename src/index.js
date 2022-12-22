77a443e817b9a4222764c5b28b9eb42c4ac3ac66
diego <dgcuenca@gmail.com>
2022-12-21 19:18:44 -0500

    Consume ID game

--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ const name = document.getElementById('name');
 const score = document.getElementById('score');
 const scoreContainer = document.getElementById('score-container');
 const leaderboard = [];
+let idgame={};
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  body: JSON.stringify({ 
+    "name": "Rhyming" 
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((gameid) => {
+    idgame = gameid;
+    console.log(idgame);
+  });
 
 btnAdd.addEventListener('click', (event) => {
   event.preventDefault();
